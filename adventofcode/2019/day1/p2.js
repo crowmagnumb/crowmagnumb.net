d8bd15abbc72a240aacb2549ca1dfaa6de5c5221
Ken Southerland <crowmagnumb@gmail.com>
2021-02-04 15:33:03 -0800

    moving

--- adventofcode/2019/day1/p2.js
+++ adventofcode/2019/day1/p2.js
@@ -1,19 +1,19 @@
-const utils = require("../../utils");
+const utils = require("../../src/utils");
 
 let total = 0;
 
-const calcFuel = mass => {
-  return Math.floor(mass / 3) - 2;
+const calcFuel = (mass) => {
+    return Math.floor(mass / 3) - 2;
 };
 
-const fn = line => {
-  let value = calcFuel(parseInt(line));
-  while (value > 0) {
-    total += value;
-    value = calcFuel(value);
-  }
+const fn = (line) => {
+    let value = calcFuel(parseInt(line));
+    while (value > 0) {
+        total += value;
+        value = calcFuel(value);
+    }
 };
 
 utils.actOnLines("2019/day1/input.txt", fn).then(() => {
-  console.log(total);
+    console.log(total);
 });
--- adventofcode/src/2020/01/s1.ts
+++ adventofcode/src/2020/01/s1.ts
@@ -1,6 +1,6 @@
 import { FileUtils } from "../../utils/index";
 
-FileUtils.readFileToLines("./input.txt").then((data) => {
+FileUtils.readFileToLines("data/2020/00/input.txt").then((data) => {
     const expenses = data.map((item) => parseInt(item));
     expenses.forEach((item, ii) => {
         for (let jj = ii + 1; jj < data.length; jj++) {
